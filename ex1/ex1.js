5cbf6f993725bf324cb19d71daa2ac736bd5e485
Tommasomerola <merolatommaso77@gmail.com>
2025-04-30 13:23:11 +0200

    ex1 complete

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -4,8 +4,25 @@ Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituis
 Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
 Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. */
 
+/* FUNCTION */
+
 function somma (a, b) {
     return a + b;
 }
 
 console.log(somma(3, 2))
+
+
+/* CONST */
+
+const somma2 = function (a, b) {
+    return a + b
+}
+
+console.log(somma2(3,9))
+
+/* ARROW FUNCTION */
+
+const somma3 = (a, b) => a+b;
+
+console.log(somma3(12, 37))
