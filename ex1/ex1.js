3f876b90c7dc325caedc0f8eefa098ccfe9b7431
Tommasomerola <merolatommaso77@gmail.com>
2025-04-30 13:15:36 +0200

    ex 1

---
+++ ex1/ex1.js
@@ -0,0 +1,11 @@
+/* 🏆 Snack 1
+Crea una funzione che somma due numeri.
+Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. */
+
+function somma (a, b) {
+    return a + b;
+}
+
+console.log(somma(3, 2))
