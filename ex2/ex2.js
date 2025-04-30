51355fcd262d59d31dd041327d19def693103300
Tommasomerola <merolatommaso77@gmail.com>
2025-04-30 13:27:37 +0200

    ex 2 complete

---
+++ ex2/ex2.js
@@ -0,0 +1,20 @@
+/* 🏆 
+🏆 Snack 2
+Crea una arrow function che calcola il quadrato di un numero.
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+ */
+
+
+/* ARROW */
+
+const quadrato = (a) => a * a;
+
+console.log(quadrato(3))
+
+/* FUNCTION */
+
+function quadrato2 (a){
+    return a * a;
+}
+
+console.log(quadrato2(5))
