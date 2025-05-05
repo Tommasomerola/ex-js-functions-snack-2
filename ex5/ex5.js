32053573616f2944c94cff6c7aba1861de44d104
Tommasomerola <merolatommaso77@gmail.com>
2025-05-05 18:30:03 +0200

    ex 5 complete

---
+++ ex5/ex5.js
@@ -0,0 +1,19 @@
+/* 🏆 Snack 5
+Crea una funzione stampaOgniSecondo con setInterval.
+Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+ */
+
+function stampaOgniSecondo(ms) {
+    return function () {
+        setInterval(()=> {
+            console.log("è passato un secondo");
+            
+        }, ms);
+    };
+}
+
+const passato1Secondo = stampaOgniSecondo(1000);
+passato1Secondo();
